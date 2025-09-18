c33c871e1da3ce162e178c4f1b2840f2eb3e8102
saisankarck <saisankarcareer039@gmail.com>
2025-09-18 21:53:10 +0530

    Add files via upload

---
+++ 05-oddOrEven.js
@@ -0,0 +1,19 @@
+// Find out odd or even
+
+// let number = 14;
+// if (number %2 ==0){
+//     console.log("Number is even")
+// }else{
+//  console.log("number is odd")
+// }
+   
+function isOddOrEven(number){
+if (number %2 ==0){
+    console.log("Number is even")
+}else{
+ console.log("number is odd")
+}
+
+}
+isOddOrEven(8)
+isOddOrEven(531)
